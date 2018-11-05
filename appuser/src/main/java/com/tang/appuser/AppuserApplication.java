e318f7172ed0665feaebf1ff02671691f36c505f
your name <your email>
2018-11-05 10:00:07 +0800

    commit

--- appuser/src/main/java/com/tang/appuser/AppuserApplication.java
+++ appuser/src/main/java/com/tang/appuser/AppuserApplication.java
@@ -2,12 +2,13 @@ package com.tang.appuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AppuserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppuserApplication.class, args);
-        System.out.println("11");
     }
 }
